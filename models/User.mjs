85e238aa345593a1dcbe344c914a7921fae4d74e
maromaroXD <amine-fb-fb@hotmail.fr>
2021-08-04 09:29:48 +0100

    User model and joi validation.

---
+++ models/User.mjs
@@ -0,0 +1,48 @@
+"use strict";
+
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  googleId : {
+    type     : String,
+    default  : null
+  },
+  email : {
+    type     : String,
+    min      : 6,
+    max      : 255
+  },
+  firstName: {
+    type     : String,
+  },
+  lastName: {
+    type     : String,
+  },
+  displayName: {
+    type     : String,
+  },
+  password : {
+    type     : String,
+    min      : 6,
+    max      : 1024
+  },
+  avatar : {
+    buffer : Buffer,
+    link   : String,
+    ext    : String
+  },
+  role : {
+    type     : String,
+  }
+},{
+  toObject: {
+    virtuals: true,
+  },
+  toJSON: {
+    virtuals: true,
+  },
+  timestamps: true
+});
+
+
+export = mongoose.model('User' , userSchema);
---
+++ models/validations/userValidation.mjs
@@ -0,0 +1,83 @@
+"use strict";
+
+const Joi = require('joi');
+
+/**
+ * Holds Google user validation.
+ *
+ * @desc Google Auth user model validation.
+ * @property {string} googleId
+ * @property {string} firstName
+ * @property {string} lastName
+ * @property {string} displayName
+ * @property {object} avatar
+ * @property {string} role
+ */
+const googleUserValidate = Joi.object({
+  googleId : Joi.string()
+                .alphanum()
+                .required(),
+  firstName: Joi.string()
+                .alphanum()
+                .required(),
+  lastName : Joi.string()
+                .alphanum()
+                .required(),
+  displayName : Joi.string()
+                   .required(),
+  avatar      : {
+       link : Joi.string(),
+  },
+  role   : Joi.string().required()
+});
+
+/**
+ * Holds Register user validation schema.
+ *
+ * @desc Local registration schema.
+ * @property {string} email
+ * @property {string} firstName
+ * @property {string} lastName
+ * @property {string} displayName
+ * @property {string} password
+ * @property {object} avatar
+ * @property {string} role
+ */
+const RegisterValidationSchema = Joi.object({
+  email : Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+  firstName: Joi.string()
+                .alphanum()
+                .required(),
+  lastName : Joi.string()
+                .alphanum()
+                .required(),
+  displayName : Joi.string()
+                .required(),
+  password : Joi.string()
+                .required(),
+  avatar : Joi.object({
+      buffer : Joi.binary().encoding('base64'),
+      ext : Joi.string()
+  }),
+  role   : Joi.string().required()
+});
+
+/**
+ * Holds Login user validation schema.
+ *
+ * @desc Local login schema.
+ * @property {string} email
+ * @property {string} password
+ */
+const LoginValidationSchema = Joi.object({
+  email : Joi.string()
+  .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+  password : Joi.string().min(6).required(),
+});
+
+export = {
+  googleUserValidate,
+  RegisterValidationSchema,
+  LoginValidationSchema
+}
